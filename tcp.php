04748c70a62065ee37a8ec0f9f792363c62a1ef6
LouisAE <opera@s4839.vps.hosting>
2022-10-04 03:12:50 +0000

    去除python服务器，改为php

---
+++ tcp.php
@@ -0,0 +1,16 @@
+<?php
+
+$datas = file_get_contents("php://input");
+
+$socket0 = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+
+socket_connect($socket0,"127.0.0.1",8081);
+
+socket_write($socket0,$datas,strlen($datas));
+
+$datar = socket_read($socket0,50);
+
+header("Content_Type:text/plain");
+
+echo $datar;
+?>
